ec9d268c113bf287671d15d9c4abe270c31619b2
sk sahabaz <sksahabaz3434@gmail.com>
2026-09-16 14:43:21 +0530

    ✅ Removing Stars From a String | Medium | Java | Runtime: 82 ms (30.26%) | Memory: 48.7 MB | 2026-09-16

---
+++ leetcode/medium/2390-removing-stars-from-a-string/solution.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public String removeStars(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '*') {
+                st.pop();
+            } else {
+                st.push(ch);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        while (!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+
+        sb.reverse();
+
+        return sb.toString();
+    }
+}
