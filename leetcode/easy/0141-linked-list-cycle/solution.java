2c09760ccc60ecb61af5ee4b9c07f9c3d94c66c0
sk sahabaz <sksahabaz3434@gmail.com>
2026-09-18 16:20:47 +0530

    ✅ Linked List Cycle | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 46.9 MB | 2026-09-18

--- leetcode/easy/0141-linked-list-cycle/solution.java
+++ leetcode/easy/0141-linked-list-cycle/solution.java
@@ -11,15 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
         ListNode fast = head;
+        ListNode slow = head;
         while(fast != null && fast.next != null){
-            slow = slow.next;
             fast = fast.next.next;
+            slow = slow.next;
             if(slow == fast){
                 return true;
             }
         }
-            return false;
+        return false;
     }
 }
