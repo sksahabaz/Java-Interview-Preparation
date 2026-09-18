e1bb414246d9dc44cec8a4824a97370bf13f39d2
sk sahabaz <sksahabaz3434@gmail.com>
2026-09-18 16:15:20 +0530

    ✅ Remove Linked List Elements | Easy | Java | Runtime: 1 ms (96.61%) | Memory: 47.2 MB | 2026-09-18

--- leetcode/easy/0203-remove-linked-list-elements/solution.java
+++ leetcode/easy/0203-remove-linked-list-elements/solution.java
@@ -11,10 +11,11 @@
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
         ListNode dummy = new ListNode(0);
+        dummy.next = head;
         ListNode curr = dummy;
         while(curr.next != null){
             if(curr.next.val == val){
-                curr = curr.next.next;
+                curr.next = curr.next.next;
             }else{
                 curr = curr.next;
             }
