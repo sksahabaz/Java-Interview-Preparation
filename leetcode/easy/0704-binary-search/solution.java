b03b03d36d78f9e395daabca654083c9e6fbda3c
sk sahabaz <sksahabaz3434@gmail.com>
2026-09-19 14:21:04 +0530

    ✅ Binary Search | Easy | Java | Runtime: 0 ms (100.00%) | Memory: 48.6 MB | 2026-09-19

--- leetcode/easy/0704-binary-search/solution.java
+++ leetcode/easy/0704-binary-search/solution.java
@@ -5,7 +5,7 @@ class Solution {
        int high = nums.length-1;
        while(low<=high){
        int mid = low+(high-low)/2;
-       if(nums[mid] == mid){
+       if(nums[mid] == target){
         return mid;
        }else if(nums[mid] < target){
         low = mid+1;
